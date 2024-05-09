b2f09c1a6422d290fd37d38135a68b80703e26fb
hujinyou <hujinyou@itiger.com>
2024-05-10 00:13:27 +0800

    upd: 增加variable value 的支持

--- Sources/SFSymbols/Extension/SwiftUIImageExtension.swift
+++ Sources/SFSymbols/Extension/SwiftUIImageExtension.swift
@@ -10,13 +10,21 @@
 
 import SwiftUI
 
-public extension Image {    
-    init(sfname: SFName) {
-        self.init(systemName: sfname.rawValue)
+public extension Image {
+    init(sfname: SFName, variableValue: Double? = nil) {
+        if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
+            self.init(systemName: sfname.rawValue, variableValue: variableValue)
+        } else {
+            self.init(systemName: sfname.rawValue)
+        }
     }
     
-    init(sfsymbol: SFSymbol) {
-        self.init(systemName: sfsymbol.rawValue)
+    init(sfsymbol: SFSymbol, variableValue: Double? = nil) {
+        if #available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *) {
+            self.init(systemName: sfsymbol.rawValue, variableValue: variableValue)
+        } else {
+            self.init(systemName: sfsymbol.rawValue)
+        }
     }
 }
 
